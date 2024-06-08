ae5826a61459a677bab59b1032afe54f9f431cc7
Pedro <pedrin.olinda@gmail.com>
2024-06-08 19:17:47 -0300

    funcao para igualar colunas

--- scripts/fusao_mercado_fev.py
+++ scripts/fusao_mercado_fev.py
@@ -2,24 +2,58 @@ import json
 import csv
 
 
-def leitura_json(path_json):
-    dados_json = []
+def reader_json(path_json):
+    data_json = []
     with open(path_json, 'r') as file:
-        dados_json = json.load(file)
-    return dados_json
+        data_json = json.load(file)
+    return data_json
 
-def leitura_csv(path_csv):
-    dados_csv = []
+def reader_csv(path_csv):
+    data_csv = []
     with open(path_csv, 'r') as file:
         # spamreader = csv.reader(file, delimiter=',')   # csv.reader criaria uma lista de listas
         spamreader = csv.DictReader(file, delimiter=',') # csv.DictReader para retorno o conteúdo do arquivo como uma lista de dicionários
         for row in spamreader:
-            dados_csv.append(row)
+            data_csv.append(row)
 
-    return dados_csv
+    return data_csv
+
+def get_header(data):
+    header = list(data[0].keys())
+    return header
+
+def rename_columns(data,key_mapping):
+    new_data_csv = []
+
+    for old_dict in data:
+        dict_temp = {}
+        for old_key, value in old_dict.items():
+            dict_temp[key_mapping[old_key]] = value
+        new_data_csv.append(dict_temp)
+
+    return new_data_csv
+  
 
 path_json = '../data_raw/dados_empresaA.json'
 path_csv = '../data_raw/dados_empresaB.csv'
 
-dados_json = leitura_json(path_json)
-dados_csv = leitura_csv(path_csv)
+
+# lendo os 2 arquivos e retornando como lista de dicionários
+data_json = reader_json(path_json)
+data_csv = reader_csv(path_csv)
+
+# mepear as chaves
+header_json = get_header(data_json)
+header_csv = get_header(data_csv)
+
+key_mapping = {
+    'Nome do Item': 'Nome do Produto',
+    'Classificação do Produto' :'Categoria do Produto',
+    'Valor em Reais (R$)':'Preço do Produto (R$)',
+    'Quantidade em Estoque':'Quantidade em Estoque',
+    'Nome da Loja':'Filial',
+    'Data da Venda':'Data da Venda'
+}
+
+data_csv = rename_columns(data_csv, key_mapping)
+print(data_csv[0])
